1e0e90cab0cff80dea166cc07a734c7564fcdafd
Valmir Farias <valmir751@hotmail.com>
2021-10-22 23:22:39 -0300

    Creating the category and snack classes

---
+++ LojaWebLanchonete/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaWebLanchonete.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public stryProperty { get; set; }
+    }
+}
--- LojaWebLanchonete/Models/Lanche.cs
+++ LojaWebLanchonete/Models/Lanche.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace LojaWebLanchonete.Models
+﻿namespace LojaWebLanchonete.Models
 {
     public class Lanche
     {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
     }
 }
